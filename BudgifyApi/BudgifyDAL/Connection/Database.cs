541b4d409cd0e7bffe43c14c4aa94d4a0e8d17b2
darckronoz <crispipe14@gmail.com>
2023-04-25 11:04:41 -0500

    basic conection structure created

--- BudgifyApi/BudgifyDAL/Connection/Database.cs
+++ BudgifyApi/BudgifyDAL/Connection/Database.cs
@@ -1,6 +1,24 @@
+using BudgifyModels;
+using Npgsql;
+
 namespace BudgifyDal.Connection;
 
 public class Database
 {
-    
+    private string conectString = "Server=144.22.175.160;Port=5432;Database=BudgifyDbTest;User Id=postgres;Password=budgifyPASS";
+
+    public Response<string> Connect()
+    {
+        Response<String> response = new Response<string>();
+        try
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(conectString);
+            conn.Open();
+        }
+        catch (NpgsqlException e) {
+            response.message = ("There was an error connecting to the database, error: "+e.ToString());
+            
+        }
+        return response;
+    }
 }
