4f61cd71de5445b41d29a2839027c67e329bbc39
darckronoz <crispipe14@gmail.com>
2023-04-25 11:48:53 -0500

    fixed basic sctructure

--- BudgifyApi/BudgifyDAL/Connection/Database.cs
+++ BudgifyApi/BudgifyDAL/Connection/Database.cs
@@ -5,15 +5,16 @@ namespace BudgifyDal.Connection;
 
 public class Database
 {
-    private readonly string _connectString = "Server=144.22.175.160;Port=5432;Database=BudgifyDbTest;User Id=postgres;Password=budgifyPASS";
+    private readonly string _connectString =
+        "Server=144.22.175.160;Port=5432;Database=BudgifyDbTest;User Id=postgres;Password=budgifyPASS";
 
-    public Response<string> Connect()
+    public async Task<Response<String>> Connect()
     {
         Response<String> response = new Response<string>();
         try
         {
-            NpgsqlConnection conn = new NpgsqlConnection(_connectString);
-            conn.Open();
+            await using var conn = new NpgsqlConnection(_connectString);
+            await conn.OpenAsync();
         }
         catch (NpgsqlException e) {
             response.message = ("There was an error connecting to the database, error: "+e);
