8c5b8323d31134c641ffef3fbf10a49e782a2d23
darckronoz <crispipe14@gmail.com>
2023-05-07 18:40:59 -0500

    added session object and some empty classes to fill

---
+++ BudgifyApi/BudgifyModels/Session.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgifyModels
+{
+    public class Session //Only for inside use - not To DataBase
+    {
+        public int Id { get; set; }
+        public string? User_id { get; set; }
+        public string? Budget_id { get; set; }
+        public Category[]? Categories { get; set; }
+        public Income[]? Incomes { get; set; }
+        public Expense[]? Expenses { get; set; }
+    }
+}
