91c4cf223d6b612c17902092249448b9be0d5b41
darckronoz <crispipe14@gmail.com>
2023-05-07 21:10:30 -0500

    fix user_id on session class

--- BudgifyApi/BudgifyModels/Session.cs
+++ BudgifyApi/BudgifyModels/Session.cs
@@ -5,7 +5,7 @@ namespace BudgifyModels
     public class Session //Only for inside use - not To DataBase
     {
         public int Id { get; set; }
-        public string? User_id { get; set; }
+        public int? User_id { get; set; }
         public string? Budget_id { get; set; }
         public Category[]? Categories { get; set; }
         public Income[]? Incomes { get; set; }
