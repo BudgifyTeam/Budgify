dc428736d1452dcb2210eb1f9989b3dd2787a5a3
darckronoz <crispipe14@gmail.com>
2023-05-11 11:48:40 -0500

    sessio object added

---
+++ BudgifyApi/BudgifyModels/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgifyModels
+{
+    internal class Session
+    {
+        public int UserId { get; set; }
+        public Budget? Budget { get; set; }
+        public Category[]? Categories { get; set; }
+        public Expense[]? Expenses { get; set; }
+        public Income[]? Incomes { get; set;}
+    }
+}
